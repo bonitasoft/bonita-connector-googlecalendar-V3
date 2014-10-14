bebb930c8be32685e1c41c1ad55422a32f12328d
Charles Souillard <charles.souillard@bonitasoft.com>
2014-10-14 14:03:28 +0200

    improve integration test

--- bonita-connector-google-calendar-v3-test/src/main/java/org/bonitasoft/connectors/google/calendar/common/GoogleCalendarV3IntegrationTests.java
+++ bonita-connector-google-calendar-v3-test/src/main/java/org/bonitasoft/connectors/google/calendar/common/GoogleCalendarV3IntegrationTests.java
@@ -18,9 +18,13 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.google.api.services.calendar.model.Event;
+
 public class GoogleCalendarV3IntegrationTests {
 
     public static final String APPLICATION_NAME = GoogleCalendarV3IntegrationTests.class.getName();
+
+    public static final boolean PRINT_OUTPUT = false;
 
     static {
         System.setProperty("gg.calendar.v3.serviceAccountId", "831328470067-dn65s17e861m08mc94820ffc2u2fju9e@developer.gserviceaccount.com");
@@ -104,30 +108,75 @@ public class GoogleCalendarV3IntegrationTests {
         return currentTime;
     }
 
-    @Test
-    public void shoudWork() throws Exception {
-        createEvent();
+    protected String getCurrentDay() {
+        final GregorianCalendar gCal = new GregorianCalendar();
+        gCal.setTime(new Date());
+        final String currentDay = Integer.toString(gCal.get(Calendar.YEAR)) + "-" + Integer.toString(gCal.get(Calendar.MONTH) + 1) + "-"
+                + Integer.toString(gCal.get(Calendar.DAY_OF_MONTH));
+        return currentDay;
     }
 
-    protected String createEvent() throws ConnectorValidationException, ConnectorException {
-        final Map<String, Object> inputParameters = getInputParameters();
-        // Main
-        inputParameters.put("startDate", "2014-10-15");
-        inputParameters.put("startTime", "14:00");
-        inputParameters.put("startTimeZone", "Europe/Paris");
-        inputParameters.put("endDate", "2014-10-15");
-        inputParameters.put("endTime", "10:00");
-        inputParameters.put("endTimeZone", "America/Los_Angeles");
-        inputParameters.put("allDay", false);
+    @Test
+    public void shoudWork() throws Exception {
+        final Map<String, Object> basicInputParameters = getInputParameters();
 
-        inputParameters.put("summary", "Test-" + getCurrentTime());
+        final String currentDay = getCurrentDay();
+        final String summary = "Test-" + currentDay + "-" + getCurrentTime();
+        final String originalStartTime = "14:00";
+        final String startTimeZone = "Europe/Paris";
+        final String originalEndTime = "10:00";
+        final String endTimeZone = "America/Los_Angeles";
 
-        final CreateEventConnector connector = new CreateEventConnector();
-        final Map<String, Object> outputParameters = executeConnector(connector, inputParameters, true);
+        // create
+        final Map<String, Object> createInputParameters = new HashMap<String, Object>(basicInputParameters);
+        createInputParameters.put(CreateEventConnector.INTPUT_START_DATE, currentDay);
+        createInputParameters.put(CreateEventConnector.INTPUT_START_TIME, originalStartTime);
+        createInputParameters.put(CreateEventConnector.INTPUT_START_TIME_ZONE, startTimeZone);
+        createInputParameters.put(CreateEventConnector.INTPUT_END_DATE, currentDay);
+        createInputParameters.put(CreateEventConnector.INTPUT_END_TIME, originalEndTime);
+        createInputParameters.put(CreateEventConnector.INTPUT_END_TIME_ZONE, endTimeZone);
+        createInputParameters.put(CreateEventConnector.INTPUT_ALL_DAY, false);
+        createInputParameters.put(CreateEventConnector.INTPUT_SUMMARY, summary);
+        final CreateEventConnector createEventConnector = new CreateEventConnector();
+        final Map<String, Object> createOutputParameters = executeConnector(createEventConnector, createInputParameters, PRINT_OUTPUT);
+        final Event createdEvent = (Event) createOutputParameters.get(CreateEventConnector.OUTPUT_EVENT);
+        Assert.assertNotNull(createdEvent);
+        Assert.assertEquals(summary, createdEvent.getSummary());
 
-        Assert.assertNotNull(outputParameters.get("event"));
+        // get
+        getEvent(basicInputParameters, getCalendarId(), createdEvent.getId(), summary);
 
-        return (String) outputParameters.get(CreateEventConnector.INTPUT_ID);
+        // update
+        final String updatedSummary = summary + "-" + getCurrentTime();
+        final Map<String, Object> updateInputParameters = new HashMap<String, Object>(basicInputParameters);
+        updateInputParameters.put(BuildEventConnector.INTPUT_ID, createdEvent.getId());
+        updateInputParameters.put(BuildEventConnector.INTPUT_SUMMARY, updatedSummary);
+        final UpdateEventConnector connector = new UpdateEventConnector();
+        final Map<String, Object> updateOutputParameters = executeConnector(connector, updateInputParameters, PRINT_OUTPUT);
+        final Event updatedEvent = (Event) updateOutputParameters.get(UpdateEventConnector.OUTPUT_EVENT);
+        Assert.assertNotNull(updatedEvent);
+        Assert.assertEquals(updatedSummary, updatedEvent.getSummary());
+
+        // get
+        getEvent(basicInputParameters, getCalendarId(), createdEvent.getId(), updatedSummary);
+
+        // move
+
+        // get
+
+        // delete
+    }
+
+    protected void getEvent(final Map<String, Object> basicInputParameters, final String calendarId, final String eventId, final String expectedSummary)
+            throws ConnectorValidationException, ConnectorException {
+        final Map<String, Object> getInputParameters = new HashMap<String, Object>(basicInputParameters);
+        getInputParameters.put(CalendarConnector.CALENDAR_ID, getCalendarId());
+        getInputParameters.put(GetEventConnector.INPUT_ID, eventId);
+        final GetEventConnector getEventConnector = new GetEventConnector();
+        final Map<String, Object> getOutputParameters = executeConnector(getEventConnector, getInputParameters, PRINT_OUTPUT);
+        final Event retrievedEvent = (Event) getOutputParameters.get(GetEventConnector.OUTPUT_EVENT);
+        Assert.assertNotNull(retrievedEvent);
+        Assert.assertEquals(expectedSummary, retrievedEvent.getSummary());
     }
 
     protected void deleteEvent(final String calendarId, final String eventId) throws ConnectorValidationException, ConnectorException {
@@ -137,42 +186,18 @@ public class GoogleCalendarV3IntegrationTests {
         inputParameters.put("id", eventId);
 
         final DeleteEventConnector connector = new DeleteEventConnector();
-        executeConnector(connector, inputParameters, true);
-    }
-
-    protected void getEvent(final String calendarId, final String eventId, final String timeZone) throws ConnectorValidationException, ConnectorException {
-        final Map<String, Object> inputParameters = getInputParameters();
-
-        inputParameters.put("calendarId", calendarId);
-        inputParameters.put("id", eventId);
-        inputParameters.put("timeZone", timeZone);
-
-        final GetEventConnector connector = new GetEventConnector();
-        executeConnector(connector, inputParameters, true);
+        executeConnector(connector, inputParameters, PRINT_OUTPUT);
     }
 
     protected void moveEvent(final String calendarId, final String eventId, final String destCalendarId) throws ConnectorValidationException,
             ConnectorException {
-        final Map<String, Object> inputParameters = getInputParameters();
+        final Map<String, Object> moveInputParameters = getInputParameters();
 
-        inputParameters.put("calendarId", calendarId);
-        inputParameters.put("id", eventId);
-        inputParameters.put("destCalendarId", destCalendarId);
+        moveInputParameters.put("id", eventId);
+        moveInputParameters.put("destCalendarId", destCalendarId);
 
         final MoveEventConnector connector = new MoveEventConnector();
-        executeConnector(connector, inputParameters, true);
+        executeConnector(connector, moveInputParameters, PRINT_OUTPUT);
     }
 
-    protected void updateEvent(final String calendarId, final String eventId) throws ConnectorValidationException, ConnectorException {
-        final Map<String, Object> inputParameters = getInputParameters();
-
-        inputParameters.put("calendarId", calendarId);
-        inputParameters.put("id", eventId);
-
-        inputParameters.put("summary", "updatedSummary");
-        inputParameters.put("location", "updatedLocation");
-
-        final UpdateEventConnector connector = new UpdateEventConnector();
-        executeConnector(connector, inputParameters, true);
-    }
 }
