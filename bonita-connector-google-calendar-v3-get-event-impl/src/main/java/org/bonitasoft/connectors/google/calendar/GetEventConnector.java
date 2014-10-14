63c4f44025173996703c4a8c5868615650806433
Nicolas Chabanoles <nicolas.chabanoles@bonitasoft.com>
2014-10-14 11:01:08 +0200

    Added Unit test on GetEventConnector: ensure the right event is fetched based on its ID and calendarID

--- bonita-connector-google-calendar-v3-get-event-impl/src/main/java/org/bonitasoft/connectors/google/calendar/GetEventConnector.java
+++ bonita-connector-google-calendar-v3-get-event-impl/src/main/java/org/bonitasoft/connectors/google/calendar/GetEventConnector.java
@@ -15,6 +15,29 @@ public class GetEventConnector extends CalendarConnector {
 
     private static final List<String> AVAILABLE_TZ_IDS = Arrays.asList(TimeZone.getAvailableIDs());
 
+    public static final String INPUT_ID = "id";
+    public static final String INPUT_PRETTY_PRINT = "prettyPrint";
+    public static final String INPUT_MAX_ATTENDEES = "maxAttendees";
+    public static final String INPUT_TIME_ZONE = "timeZone";
+
+    public static final String OUTPUT_EVENT = "event";
+    public static final String OUTPUT_ETAG = "etag";
+    public static final String OUTPUT_HANGOUT_LINK = "hangoutLink";
+    public static final String OUTPUT_HTML_LINK = "htmlLink";
+    public static final String OUTPUT_I_CAL_UID = "iCalUID";
+    public static final String OUTPUT_ID = "id";
+    public static final String OUTPUT_STATUS = "status";
+    public static final String OUTPUT_SUMMARY = "summary";
+    public static final String OUTPUT_DESCRIPTION = "description";
+    public static final String OUTPUT_LOCATION = "location";
+    public static final String OUTPUT_TRANSPARENCY = "transparency";
+    public static final String OUTPUT_VISIBILITY = "visibility";
+    public static final String OUTPUT_SEQUENCE = "sequence";
+    public static final String OUTPUT_ANYONE_CAN_ADD_SELF = "anyoneCanAddSelf";
+    public static final String OUTPUT_GUESTS_CAN_INVITE_OTHERS = "guestsCanInviteOthers";
+    public static final String OUTPUT_GUESTS_CAN_MODIFY = "guestsCanModify";
+    public static final String OUTPUT_GUESTS_CAN_SEE_OTHER_GUESTS = "guestsCanSeeOtherGuests";
+
     @Override
     protected List<String> checkParameters() {
         final List<String> errors = new ArrayList<String>();
@@ -43,40 +66,45 @@ public class GetEventConnector extends CalendarConnector {
         }
 
         final Event event = get.execute();
-        setOutputParameter("event", event);
-        setOutputParameter("etag", event.getEtag());
-        setOutputParameter("hangoutLink", event.getHangoutLink());
-        setOutputParameter("htmlLink", event.getHtmlLink());
-        setOutputParameter("iCalUID", event.getICalUID());
-        setOutputParameter("id", event.getId());
-        setOutputParameter("status", event.getStatus());
-        setOutputParameter("summary", event.getSummary());
-        setOutputParameter("description", event.getDescription());
-        setOutputParameter("location", event.getLocation());
-        setOutputParameter("transparency", event.getTransparency());
-        setOutputParameter("visibility", event.getVisibility());
-        setOutputParameter("sequence", event.getSequence());
-        setOutputParameter("anyoneCanAddSelf", event.getAnyoneCanAddSelf());
-        setOutputParameter("guestsCanInviteOthers", event.getGuestsCanInviteOthers());
-        setOutputParameter("guestsCanModify", event.getGuestsCanModify());
-        setOutputParameter("guestsCanSeeOtherGuests", event.getGuestsCanSeeOtherGuests());
 
+        setOutputParameters(event);
+
+    }
+
+    protected void setOutputParameters(Event event) {
+        setOutputParameter(OUTPUT_EVENT, event);
+        setOutputParameter(OUTPUT_ETAG, event.getEtag());
+        setOutputParameter(OUTPUT_HANGOUT_LINK, event.getHangoutLink());
+        setOutputParameter(OUTPUT_HTML_LINK, event.getHtmlLink());
+        setOutputParameter(OUTPUT_I_CAL_UID, event.getICalUID());
+        setOutputParameter(OUTPUT_ID, event.getId());
+        setOutputParameter(OUTPUT_STATUS, event.getStatus());
+        setOutputParameter(OUTPUT_SUMMARY, event.getSummary());
+        setOutputParameter(OUTPUT_DESCRIPTION, event.getDescription());
+        setOutputParameter(OUTPUT_LOCATION, event.getLocation());
+        setOutputParameter(OUTPUT_TRANSPARENCY, event.getTransparency());
+        setOutputParameter(OUTPUT_VISIBILITY, event.getVisibility());
+        setOutputParameter(OUTPUT_SEQUENCE, event.getSequence());
+        setOutputParameter(OUTPUT_ANYONE_CAN_ADD_SELF, event.getAnyoneCanAddSelf());
+        setOutputParameter(OUTPUT_GUESTS_CAN_INVITE_OTHERS, event.getGuestsCanInviteOthers());
+        setOutputParameter(OUTPUT_GUESTS_CAN_MODIFY, event.getGuestsCanModify());
+        setOutputParameter(OUTPUT_GUESTS_CAN_SEE_OTHER_GUESTS, event.getGuestsCanSeeOtherGuests());
     }
 
     private String getTimeZone() {
-        return (String) getInputParameter("timeZone");
+        return (String) getInputParameter(INPUT_TIME_ZONE);
     }
 
     protected Integer getMaxAttendees() {
-        return (Integer) getInputParameter("maxAttendees");
+        return (Integer) getInputParameter(INPUT_MAX_ATTENDEES);
     }
 
     private String getId() {
-        return (String) getInputParameter("id");
+        return (String) getInputParameter(INPUT_ID);
     }
 
     protected Boolean getPrettyPrint() {
-        return (Boolean) getInputParameter("prettyPrint");
+        return (Boolean) getInputParameter(INPUT_PRETTY_PRINT);
     }
 
 }
--- bonita-connector-google-calendar-v3-get-event-impl/src/test/java/org/bonitasoft/connectors/google/calendar/GetEventConnectorTest.java
+++ bonita-connector-google-calendar-v3-get-event-impl/src/test/java/org/bonitasoft/connectors/google/calendar/GetEventConnectorTest.java
@@ -1,14 +1,47 @@
 package org.bonitasoft.connectors.google.calendar;
 
-import org.junit.Ignore;
+import com.google.api.services.calendar.Calendar;
+import org.bonitasoft.connectors.google.calendar.common.CalendarConnector;
+import com.google.api.services.calendar.model.Event;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class GetEventConnectorTest {
 
-    @Ignore
     @Test
-    public void shoudWork() throws Exception {
+    public void should_DoJobWithCalendar_Get_right_event_based_on_id() throws Exception {
 
+        // Given
+        GetEventConnector spyGetEventConnector = Mockito.spy(new GetEventConnector());
+        String calendarId = "Calendar Identifier";
+        String id = "Event Identifier";
+
+        Map<String, Object> inputParameters = new HashMap<String, Object>();
+        inputParameters.put(CalendarConnector.CALENDAR_ID, calendarId);
+        inputParameters.put(GetEventConnector.INPUT_ID, id);
+        spyGetEventConnector.setInputParameters(inputParameters);
+
+        Calendar mockCalendarService = Mockito.mock(Calendar.class);
+        Calendar.Events mockedEvents = Mockito.mock(Calendar.Events.class);
+        Mockito.when(mockCalendarService.events()).thenReturn(mockedEvents);
+
+        Calendar.Events.Get get = Mockito.mock(Calendar.Events.Get.class);
+        Mockito.when(mockedEvents.get(calendarId, id)).thenReturn(get);
+
+        Mockito.doNothing().when(spyGetEventConnector).setOutputParameters(Mockito.any(Event.class));
+
+
+        // When
+        spyGetEventConnector.doJobWithCalendar(mockCalendarService);
+
+        // Then
+        Mockito.verify(mockedEvents).get(calendarId, id);
     }
+
+
 
 }
