9577a27dcc3da11ffbdaf37c90e5ebd6f44e4993
Romain Bioteau <romain.bioteau@bonitasoft.com>
2018-12-19 17:09:58 +0100

    feat(java11) update project layout
    execute tests against java 11

---
+++ src/test/java/org/bonitasoft/connectors/google/calendar/DeleteEventConnectorTest.java
@@ -0,0 +1,46 @@
+package org.bonitasoft.connectors.google.calendar;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bonitasoft.connectors.google.calendar.common.CalendarConnector;
+import org.junit.Test;
+
+import com.google.api.services.calendar.Calendar;
+
+public class DeleteEventConnectorTest {
+
+    @Test
+    public void should_DoJobWithCalendar_Delete_right_event_based_on_id() throws Exception {
+
+        // Given
+        DeleteEventConnector connector = new DeleteEventConnector();
+        String calendarId = "Calendar Identifier";
+        String id = "Event Identifier";
+
+        Map<String, Object> inputParameters = new HashMap<String, Object>();
+        inputParameters.put(CalendarConnector.CALENDAR_ID, calendarId);
+        inputParameters.put(CalendarConnector.INPUT_ID, id);
+        connector.setInputParameters(inputParameters);
+
+        Calendar.Events mockedEvents = mock(Calendar.Events.class);
+
+        Calendar.Events.Delete mockDeleteRequest = mock(Calendar.Events.Delete.class);
+        when(mockedEvents.delete(calendarId, id)).thenReturn(mockDeleteRequest);
+
+        Calendar.Events.Get mockGetRequest = mock(Calendar.Events.Get.class);
+        when(mockedEvents.get(calendarId, id)).thenReturn(mockGetRequest);
+
+        // When
+        connector.doJobWithCalendarEvents(mockedEvents);
+
+        // Then
+        verify(mockDeleteRequest).execute();
+        verify(mockGetRequest).execute();
+    }
+
+}
